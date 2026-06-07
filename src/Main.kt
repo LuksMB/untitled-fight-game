b062eeafc584fb78d48bc7029ac46c677324c01f
Camila Belmont <camilabelmont.alu@gmail.com>
2026-06-07 13:32:07 -0300

    Creditos finalizado

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,12 @@
 fun main() {
     val menu = Menu()
+    val credits = Credits()
         while (true) {
             menu.showMenu()
             when (readln()) {
                 "1" -> println("Iniciando jogo...")
                 "2" -> println("Personagens")
-                "3" -> println("Créditos")
+                "3" -> credits.showCredits()
                 "0" -> {
                     println("Até logo!")
                     break
